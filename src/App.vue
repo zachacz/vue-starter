9d7b06510da442ea4ade8e3d877761d0e7d5eb7c
zachacz <zachacz@d17-430-pc-09.ipa.ii.agh.edu.pl>
2023-04-16 14:50:40 +0200

    App login logout

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <template>
   <div class="my-app">
     <h1>Witaj w systemie do zapisów na zajęcia</h1>
-    <h1>Twój e-mail to {{ email }}</h1>
-    <input type="email" v-model="email">
-    <div v-if="!email.includes('@')">Twój email jest nieprawidlowy!</div>
-    <div v-else-if="email.length < 10">Ale masz krótki adres!</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    {{email.length }}
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <div v-if="loggedIn">Zalogowany {{ email }}
+    <a @click="logMeOut()">Wyloguj</a>
+
+    </div>
+    <div v-else>
+      <input type="email" v-model="email">
+      <button @click="logMeIn()">Zaloguj się</button>
+      <div v-if="!email.includes('@')">Twój email jest nieprawidlowy!</div>
+      <div v-else-if="email.length < 10">Ale masz krótki adres!</div>
+      <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+      <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+      {{email.length }}
+    </div>
   </div>
 </template>
 
 <script>
+import "milligram";
+
 export default {
 data() {
   return {
     email: 'mateusz.zachacz@gmail.com',
+    loggedIn: false,
     password: ''
   };
 },
   methods: {
-    alertMyEmail() {
-      alert(this.email);
-    }
+    logMeIn() {
+      this.loggedIn = true;
+    },
+    logMeOut() {
+      this.loggedIn = false;
+    },
   }
-
-
 }
+
 </script>
 
 <style>
